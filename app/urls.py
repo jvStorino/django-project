f8f3205e1dffb705383d432bf5e01edbcac9fe1f
Storino <Storinojv@gmail.com>
2024-10-28 18:04:23 -0300

    criando PUT

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home, cadastrar
+from .views import home, cadastrar, editar, update
 
 urlpatterns = [
     path('', home, name='home'),
-    path('cadastrar/', cadastrar, name='cadastrar')
+    path('cadastrar/', cadastrar, name='cadastrar'),
+    path('editar/<int:id>', editar, name='editar'),
+    path('update/<int:id>', update, name='update')
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Pessoa
 
 def home(request):
@@ -10,3 +10,14 @@ def cadastrar(request):
     Pessoa.objects.create(nome=_nome)
     pessoas = Pessoa.objects.all()
     return render(request, 'index.html', {'pessoas': pessoas})
+
+def editar(request, id):
+    pessoa = Pessoa.objects.get(id=id)
+    return render(request, 'update.html', {'pessoa': pessoa})
+
+def update(request, id):
+    _nome = request.POST.get('nome')
+    pessoa = Pessoa.objects.get(id=id)
+    pessoa.nome = _nome
+    pessoa.save()
+    return redirect(home)
